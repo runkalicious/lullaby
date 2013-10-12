cb2b777f97f47a5d0bcb81ddd9e29428d2abb498
Matt Runkle <matt@mrunkle.com>
2013-10-12 15:59:55 -0400

    fix truthiness return value

--- connectors/googlemusic.js
+++ connectors/googlemusic.js
@@ -6,7 +6,7 @@ function l_isPlaying() {
 	// Google adds the class 'playing' to the play/pause button
 	// when the player is active. The class is removed when paused
 	// or not active.
-	if ( $('#player').find('button.playing') )
+	if ( $('#player').find('button.playing').length > 0 )
 		return true;
 	
 	return false;
