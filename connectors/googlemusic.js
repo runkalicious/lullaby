c987b052bb54847990b56e83edc9f52c56add808
Matt Runkle <matt@mrunkle.com>
2013-10-12 15:37:27 -0400

    Implement the Google Music connector.

--- connectors/googlemusic.js
+++ connectors/googlemusic.js
@@ -2,12 +2,20 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-console.log("I AM GOOGLE MUSIC");
-
 function l_isPlaying() {
-	return true;
+	// Google adds the class 'playing' to the play/pause button
+	// when the player is active. The class is removed when paused
+	// or not active.
+	if ( $('#player').find('button.playing') )
+		return true;
+	
+	return false;
 }
 
 function l_pause() {
 	console.log("Pausing Google Play");
+	
+	// Get pause button
+	var pauseButton = $('#player').find('button.playing')[0];
+	$(pauseButton).click();
 }
